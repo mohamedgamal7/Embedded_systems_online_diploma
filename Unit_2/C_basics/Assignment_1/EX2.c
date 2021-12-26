d17df1abc022709f98cda703fbec3df635cfe331
mohamedgamal7 <mohamadgamal2021@gmail.com>
2021-12-26 22:41:20 +0200

    second assignment

---
+++ Unit_2/C_basics/Assignment_1/EX2.c
@@ -0,0 +1,16 @@
+/*
+ * EX1.c
+ *
+ *  Created on: Dec 26, 2021
+ *      Author: Mohamed_Gamal
+ */
+# include "stdio.h"
+
+int main ()
+{
+	int number;
+	printf("Enter an integer : ");
+	fflush(stdout); fflush(stdin);
+	scanf("%d",&number);
+	printf("You entered: %d",number);
+}
